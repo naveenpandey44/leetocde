ff6d4f1b2fd02d684233f997b26f5816ad3fffda
naveenpandey44 <166904366+naveenpandey44@users.noreply.github.com>
2025-05-22 17:57:07 +0530

    Time: 0 ms (100%), Space: 42.8 MB (46.48%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,18 +1,29 @@
-
-
 class Solution {
+    List<List<Integer>> ans ;
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), nums, 0);
-        return result;
+        ans = new ArrayList<>();
+        int n = nums.length;
+        fun(nums, n, new ArrayList<Integer>());
+        return ans;
     }
-    
-    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums, int start) {
-        result.add(new ArrayList<>(tempList));
-        for (int i = start; i < nums.length; i++) {
-            tempList.add(nums[i]);               
-            backtrack(result, tempList, nums, i + 1); 
-            tempList.remove(tempList.size() - 1); 
+        public void fun(int nums[] , int n , ArrayList<Integer>temp) {
+        if(n==0){
+            // add the temp to ans list
+           ArrayList<Integer> subset = new ArrayList<>(); 
+           for(Integer a : temp)
+           subset.add(a);
+           ans.add(subset);
+           return;  
+                   
+           
         }
+        // include
+        temp.add(nums[n-1]);
+        fun(nums, n-1, temp);
+        temp.remove(temp.size()-1);
+
+        // exclude
+        fun(nums, n-1, temp);
+        
     }
 }
